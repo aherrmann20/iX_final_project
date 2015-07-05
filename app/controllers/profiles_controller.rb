7570a09b146e13b8ca16789e4bb86b8ed907f24c
aherrmann20 <aherrmann@college.harvard.edu>
2015-07-05 21:32:05 +0200

    Added devise and functions to the profiles controller. Having difficulty with devise

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,23 +1,46 @@
 class ProfilesController < ApplicationController
+  before_action :set_user
+  
   def index
   end
 
   def new
+    @profile = Profie.new
   end
 
   def create
+    @profile = Profile.new(profile_params)
+    @profile.user = @user
+
+    if @profile.save
+      redirect_to @profile
+    else
+      render 'new'
+    end
   end
 
   def show
   end
 
   def edit
+    @profile = Profile.find params[:id]
   end
 
   def update
+    @profile = Profile.find params[:id]
+
+    if @profile.update(profile_params)
+      redirect_to @profile, notice: "Your profile was successfully updated."
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @profile = Profile.find params[:id]
+    @profile.destroy
+
+    redirect_to welcome_index_path
   end
 
   private
