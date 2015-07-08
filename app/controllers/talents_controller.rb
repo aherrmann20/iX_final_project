ed972dca743d9d530bc218a254149cfdfbb1a278
aherrmann20 <aherrmann@college.harvard.edu>
2015-07-08 12:19:19 +0200

    Finalized functionality of search

--- app/controllers/talents_controller.rb
+++ app/controllers/talents_controller.rb
@@ -7,8 +7,17 @@ class TalentsController < ApplicationController
 		@talents = Talent.all.order('created_at DESC')
 	end
 
+	def show
+		@talent = Talent.find params[:id]
+	end
+
 	def search
 		@talents = Talent.search(params[:search]).order("created_at DESC")
 		render :index
 	end
+
+	private
+	def talent_params
+    params.require(:talent).permit(profile_ids: [])
+  end
 end
