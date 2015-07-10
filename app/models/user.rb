fd2786c8dda208ea1466f6226610fda744a6a229
aherrmann20 <aherrmann@college.harvard.edu>
2015-07-10 14:16:15 +0200

    Remove dependent destroy from user profile temporarily

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
 	devise :database_authenticatable, :registerable,
 	         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
-	has_one :profile, dependent: :destroy
+	has_one :profile
 	has_many :ratings
 
 	def self.from_omniauth(auth)
